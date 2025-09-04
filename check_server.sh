0e263dc922b38c2c1b869b65483493b804da7eba
Jinsoo Lim <jinsoo@jinsooui-MacBookPro.local>
2025-09-04 23:06:23 +0900

    feat: Add initial server check script

---
+++ check_server.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# Simple script to print current data and a message.
+
+echo "================================"
+echo "Helo world! Starting server check..."
+echo "Current time is: $(date)"
+echo "================================"
+
