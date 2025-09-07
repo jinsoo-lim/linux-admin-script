bf96a1097e4467e3171dc7b36a2f12c7e188b5cd
jinsoo-lim <jinsuzinsu@gmail.com>
2025-09-07 16:40:10 +0900

    feat: Add backup and old file check logic

--- check_server.sh
+++ check_server.sh
@@ -14,7 +14,7 @@ current_time=$(date)
 disk_usage=$(df / | tail -1 | awk '{pring $5}')
 
 # usage memory(All of usage %)
-mem_usage=$(free | awk '/Mem/ {printf("%.0f%%", $3/$2 * 100)}')
+mem_usage=$(free | awk '/Mem/ {printf "%.0f%%", $3/$2 * 100}')
 
 # System uptime
 uptime_info=$(uptime -p)
@@ -28,3 +28,30 @@ echo "Disk Usage: $disk_usage"
 echo "Memory Usage: $mem_usage"
 echo "uptime:       $uptime_info"
 echo "========================================="
+
+# =============================================
+# week 3 : automatic backup, delete old file.
+# =============================================
+
+# backup diretory
+backup_dir="/var/log"
+backup_dest="/tmp/log_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+# if exsist dir, compression
+if [ -d "$backup_dir" ]; then
+	echo "Backing up $backup_dir to $backup_dest..."
+	tar -czf "$backup_dest" "$backup_dir"
+	echo "Backup complete!"
+else
+	echo "Diretory $backup_dir does not exist."
+fi
+
+# process delete old file (over 7day)
+echo
+echo "Checking for files older than 7 days in $backup_dir ..."
+for file in $(find "$backup_dir" -type f -mtime 7); do
+	# print safety instead of real delete
+	echo "Would delete : $file"
+done
+
+echo "Script finished!"
